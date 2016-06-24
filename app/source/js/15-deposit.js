986eb3fbedf78f8d57f2a28c0a2dfec769656aaa
Iyad Bitar <iyadbitar@Iyads-MacBook-Pro.local>
2016-06-24 09:53:33 -0400

    Credit card detection added + deposit form validation fix

--- app/source/js/15-deposit.js
+++ app/source/js/15-deposit.js
@@ -20,6 +20,11 @@
          */
         var isContinueClicked = false;
 
+        /**
+         * hold credit card type after detection
+         */
+        var cardType;
+
 
         /**
          * initialize form validation
@@ -79,5 +84,97 @@
         .on('keyup', formatCardDate);
 
 
+        /**
+         * Credit card form is not required but user enter a value in one filed the form should be validated
+         */
+        var ccFileds = $('.cc-field.cc-to-be-validate input').on('keyup change', function(ev){
+            var include = false;
+            ccFileds.each(function(n){
+                include = include || $(this).val().length > 0;
+                return !include; //// no need to continue .each if include is true
+            });
+
+            if(true === include){
+                //// cc data should be validate
+                myForm.find('.cc-to-be-validate').addClass('cc-validate');
+            }
+            else{
+                //// no field has data, remove validation
+                myForm.find('.cc-to-be-validate').removeClass('cc-validate');
+                resetFields(myForm);
+            }
+        });
+
+        /**
+         * Detect credit card type on keyup event
+         */
+        $('input#cc_cardnumber').on('keyup', function(ev){
+            var val = $(this).val();
+            var type = detectCardType(val);
+
+            if(val.length >3){
+                if(true === !!type ){
+                    if(type !== cardType){
+                        cardType = type;
+                        $('.cc-cards li').css('opacity', 0.4);
+                        $('.cc-cards .card-'+cardType).css('opacity', 1);
+                    }
+                }
+                else{
+                    cardType = undefined;
+                    $('.cc-cards li').css('opacity', 1);
+                }
+            }
+        });
+
+
+
+
     };//// depositReady
+
+    /**
+     * [detectCardType will return credit card type based on RegEx test]
+     * this function is copied form SackOverflow.com post
+     * http://stackoverflow.com/questions/72768/how-do-you-detect-credit-card-type-based-on-number
+     */
+    function detectCardType(number) {
+        var re = {
+            visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+            electron: /^(4026|417500|4405|4508|4844|4913|4917)\d+$/,
+            maestro: /^(5018|5020|5038|5612|5893|6304|6759|6761|6762|6763|0604|6390)\d+$/,
+            dankort: /^(5019)\d+$/,
+            interpayment: /^(636)\d+$/,
+            unionpay: /^(62|88)\d+$/,
+            mastercard: /^5[1-5][0-9]{14}$/,
+            amex: /^3[47][0-9]{13}$/,
+            diners: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
+            discover: /^6(?:011|5[0-9]{2})[0-9]{12}$/,
+            jcb: /^(?:2131|1800|35\d{3})\d{11}$/
+        };
+        if (re.electron.test(number)) {
+            return 'ELECTRON';
+        } else if (re.maestro.test(number)) {
+            return 'MAESTRO';
+        } else if (re.dankort.test(number)) {
+            return 'DANKORT';
+        } else if (re.interpayment.test(number)) {
+            return 'INTERPAYMENT';
+        } else if (re.unionpay.test(number)) {
+            return 'UNIONPAY';
+        } else if (re.visa.test(number)) {
+            return 'VISA';
+        } else if (re.mastercard.test(number)) {
+            return 'MASTERCARD';
+        } else if (re.amex.test(number)) {
+            return 'AMEX';
+        } else if (re.diners.test(number)) {
+            return 'DINERS';
+        } else if (re.discover.test(number)) {
+            return 'DISCOVER';
+        } else if (re.jcb.test(number)) {
+            return 'JCB';
+        } else {
+            return undefined;
+        }
+    }
 })();
